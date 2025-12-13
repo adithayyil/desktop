90024b6517d5924fd06c0e18438343bfb55bd63b
adithayyil <adithayyil@proton.me>
2025-12-13 17:46:18 -0500

    fix(server): sleeping

--- profiles/server/power.nix
+++ profiles/server/power.nix
@@ -6,10 +6,20 @@
 }:
 
 {
-  # Keep server running when lid is closed
+  # Keep server running - disable all power management
   services.logind.settings.Login = {
     HandleLidSwitch = "ignore";
     HandleLidSwitchDocked = "ignore";
     HandleLidSwitchExternalPower = "ignore";
+    HandlePowerKey = "ignore";
+    HandleSuspendKey = "ignore";
+    HandleHibernateKey = "ignore";
+    IdleAction = "ignore";
   };
+
+  # Disable all sleep/suspend/hibernate targets
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 }
