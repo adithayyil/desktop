5d87e88d76daa746f299147059142466868679f5
adithayyil <adithayyil@proton.me>
2025-12-25 18:40:06 -0500

    fix: dont sleep when on full screen

--- home/programs/swayidle.nix
+++ home/programs/swayidle.nix
@@ -15,16 +15,16 @@
     timeouts = [
       {
         timeout = 300;
-        command = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10%";
+        command = "${pkgs.sway}/bin/swaymsg -t get_tree | ${pkgs.jq}/bin/jq -e '.nodes[].nodes[].nodes[] | select(.focused==true and .fullscreen_mode==1)' > /dev/null || ${pkgs.brightnessctl}/bin/brightnessctl -s set 10%";
         resumeCommand = "${pkgs.brightnessctl}/bin/brightnessctl -r";
       }
       {
         timeout = 600;
-        command = "${pkgs.swaylock-effects}/bin/swaylock -f";
+        command = "${pkgs.sway}/bin/swaymsg -t get_tree | ${pkgs.jq}/bin/jq -e '.nodes[].nodes[].nodes[] | select(.focused==true and .fullscreen_mode==1)' > /dev/null || ${pkgs.swaylock-effects}/bin/swaylock -f";
       }
       {
         timeout = 900;
-        command = "${pkgs.sway}/bin/swaymsg 'output * dpms off'";
+        command = "${pkgs.sway}/bin/swaymsg -t get_tree | ${pkgs.jq}/bin/jq -e '.nodes[].nodes[].nodes[] | select(.focused==true and .fullscreen_mode==1)' > /dev/null || ${pkgs.sway}/bin/swaymsg 'output * dpms off'";
         resumeCommand = "${pkgs.sway}/bin/swaymsg 'output * dpms on'";
       }
     ];
